358a3eca3d14804132a618b5599d95dff4636443
TolosaElianaM <tolosa.eliana.m@gmail.com>
2021-09-24 14:32:24 -0300

    Se termino la vista /create y /edit

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -1,12 +1,27 @@
+const products = require('../data/products.json')
+
 const controller = {
-    list: function (req, res, next) {
+    list: function (req, res,next) {
         res.render('admin/admin');
     },
+    create: function (req, res,next) {
+        res.render('admin/create');
+    },
+    store: function (req, res,next) {
+        res.render('admin/create');
+    },
     edit: function (req, res, next) {
+        const {
+            id
+        } = req.params;
+        let producto = products.find(element => element.id == id);
+        res.render('admin/edit', {producto} );
+    },
+    update: function (req, res, next) {
         res.render('admin/edit');
     },
-    create: function (req, res, next) {
-        res.render('admin/create');
+    erase: function (req, res , next) {
+        res.redirect('/admin');
     }
 }
 module.exports = controller;
