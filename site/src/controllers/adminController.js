e5e5e33167d6d1ceb2ab676c0c057264970d75ed
Mariano <marianogutierrez90@gmail.com>
2021-09-25 01:28:29 -0300

    subo cambios de admin

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -1,13 +1,19 @@
 const products = require('../data/products.json')
 
+const path = require('path')
+const fs = require('fs')
+
 const controller = {
-    list: function (req, res,next) {
-        res.render('admin/admin');
+    list: function (req, res, next) {
+
+        let products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'), 'utf-8'));
+
+        res.render('admin/admin', {products});
     },
-    create: function (req, res,next) {
+    create: function (req, res, next) {
         res.render('admin/create');
     },
-    store: function (req, res,next) {
+    store: function (req, res, next) {
         res.render('admin/create');
     },
     edit: function (req, res, next) {
@@ -15,12 +21,14 @@ const controller = {
             id
         } = req.params;
         let producto = products.find(element => element.id == id);
-        res.render('admin/edit', {producto} );
+        res.render('admin/edit', {
+            producto
+        });
     },
     update: function (req, res, next) {
         res.render('admin/edit');
     },
-    erase: function (req, res , next) {
+    erase: function (req, res, next) {
         res.redirect('/admin');
     }
 }
